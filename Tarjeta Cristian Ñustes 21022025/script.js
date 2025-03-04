de894cc519b0ba8ff5da1cea15d7bae515ef8ff5
Cristinustes <sebascristi04@gmail.com>
2025-03-04 00:42:31 -0500

    Alerta

--- Tarjeta Cristian Ñustes 21022025/script.js
+++ Tarjeta Cristian Ñustes 21022025/script.js
@@ -1,8 +1,7 @@
 var boton = document.getElementById("boton");
+var texto =document.getElementById("contenido")
 
-function CambiarTexto(){
-    var texto=document.getElementById("contenido");
-    texto.textContent=" Profesional responsable, honesto, puntual y proactivo, con gran capacidad de adaptación y aprendizaje. Orientado al logro de objetivos, con excelentes habilidades de análisis y trabajo en equipo. ";
-}
-
-boton.addEventListener("click",CambiarTexto);
+boton.addEventListener('click', function() {
+        alert('La información se actualizará ahora.');
+        texto.textContent = " Profesional responsable, honesto, puntual y proactivo, con gran capacidad de adaptación y aprendizaje. Orientado al logro de objetivos, con excelentes habilidades de análisis y trabajo en equipo. ";
+    });
